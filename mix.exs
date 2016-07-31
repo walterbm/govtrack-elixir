febae7104e67fba1f3054decf4cc7aa7769fa128
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-07-31 00:49:16 -0400

    update mix configuration file for Hex

--- mix.exs
+++ mix.exs
@@ -2,17 +2,18 @@ defmodule Govtrack.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :govtrack,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :govtrack,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description,
+      package: package,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications:
       [
@@ -22,15 +23,6 @@ defmodule Govtrack.Mixfile do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpotion, "~> 3.0.0"},
