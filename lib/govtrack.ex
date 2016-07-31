2bedad2a57fbe1e95c81ce3651029e8c84a28f8d
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-07-31 00:49:50 -0400

    add sensible defaults for basic API requests

--- lib/govtrack.ex
+++ lib/govtrack.ex
@@ -6,46 +6,46 @@ defmodule Govtrack do
   use HTTPotion.Base
 
   @api_url "https://www.govtrack.us/api/"
-  @api_version "v2/"
+  @api_version "v2"
 
   def process_url(url) do
-    "#{@api_url}#{@api_version}" <> url
+    "#{@api_url}#{@api_version}/" <> url
   end
 
   def process_response_body(body) do
     body |> Poison.decode!
   end
 
-  def bill() do
-    get("bill")
+  def bill(query \\ %{congress: 114, order_by: "current_status_date"}) do
+    get("bill", query: query)
   end
 
-  def cosponsorship() do
+  def cosponsorship(query \\ %{}) do
     get("cosponsorship")
   end
 
-  def person() do
-    get("person")
+  def person(query \\ %{}) do
+    get("person", query: query)
   end
 
-  def role() do
-    get("role")
+  def role(query \\ %{current: true}) do
+    get("role", query: query)
   end
 
-  def vote() do
-    get("vote")
+  def vote(query \\ %{order_by: "created"}) do
+    get("vote", query: query)
   end
 
-  def vote_voter() do
-    get("vote_voter")
+  def vote_voter(query \\ %{order_by: "created"}) do
+    get("vote_voter", query: query)
   end
 
-  def committee() do
-    get("committee")
+  def committee(query \\ %{obsolete: false}) do
+    get("committee", query: query)
   end
 
-  def committee_member() do
-    get("committee_member")
+  def committee_member(query \\ %{}) do
+    get("committee_member", query: query)
   end
 
 end
