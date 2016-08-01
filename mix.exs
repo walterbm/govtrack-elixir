770ad81c7924210fc424ca91397f18e8f107c39c
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-07-31 20:12:07 -0400

    bump version to 0.5.0 for publishing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Govtrack.Mixfile do
   def project do
     [
       app: :govtrack,
-      version: "0.0.1",
+      version: "0.5.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
